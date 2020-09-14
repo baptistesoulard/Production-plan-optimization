ac64cc7f0ffbf5109a04e47a6e772de4e8be5a9c
Besnard Consulting - Rémi <contact@besnard-consulting.com>
2020-09-14 17:42:20 +0200

    Clean Model1.py's code

--- Model1/Model1.py
+++ Model1/Model1.py
@@ -12,7 +12,12 @@ from matplotlib import pyplot as plt
 from typing import List, Dict
 
 
-def optimize_planning(timeline, workcenters, needs, wc_cost):
+def optimize_planning(
+    timeline: List[str],
+    workcenters: List[str],
+    needs: Dict[str, int],
+    wc_cost: Dict[str, int],
+) -> pd.DataFrame:
     # Initiate optimization model
     model = gurobipy.Model("Optimize production planning")
 
@@ -25,7 +30,12 @@ def optimize_planning(timeline, workcenters, needs, wc_cost):
         timeline, workcenters, vtype=gurobipy.GRB.BINARY, name="Line opening"
     )
     total_hours = model.addVars(
-        timeline, workcenters, lb=0, ub=12, vtype=gurobipy.GRB.INTEGER, name="Total hours"
+        timeline,
+        workcenters,
+        lb=0,
+        ub=12,
+        vtype=gurobipy.GRB.INTEGER,
+        name="Total hours",
     )
 
     model.addConstrs(
@@ -40,7 +50,9 @@ def optimize_planning(timeline, workcenters, needs, wc_cost):
     )
 
     # Define cost variable
-    cost = model.addVars(timeline, workcenters, lb=0, vtype=gurobipy.GRB.CONTINUOUS, name="Cost")
+    cost = model.addVars(
+        timeline, workcenters, lb=0, vtype=gurobipy.GRB.CONTINUOUS, name="Cost"
+    )
 
     model.addConstrs(
         (
@@ -63,7 +75,9 @@ def optimize_planning(timeline, workcenters, needs, wc_cost):
     # Objective : minimize a function
     model.ModelSense = gurobipy.GRB.MINIMIZE
     # Function to minimize
-    optimization_var = gurobipy.quicksum(cost[(i, j)] for i in timeline for j in workcenters)
+    optimization_var = gurobipy.quicksum(
+        cost[(i, j)] for i in timeline for j in workcenters
+    )
     objective = 0
     objective += optimization_var
 
@@ -72,29 +86,33 @@ def optimize_planning(timeline, workcenters, needs, wc_cost):
     model.optimize()
     print("Total cost = $" + str(model.ObjVal))
 
-    sol = pd.DataFrame(data={"Solution": model.X}, index=model.VarName)
-    sol = sol.iloc[2 * len(cost): 3 * len(cost)]
+    solution = pd.DataFrame(data={"Solution": model.X}, index=model.VarName)
+    solution = solution.iloc[2 * len(cost) : 3 * len(cost)]
 
-    return sol
+    return solution
 
 
-def plot_planning(plan, need):
-    plan = plan.T
-    plan["Min capacity"] = 7
-    plan["Max capacity"] = 12
+def plot_planning(planning: pd.DataFrame, needs: pd.DataFrame) -> None:
+    planning = planning.T
+    planning["Min capacity"] = 7
+    planning["Max capacity"] = 12
 
     my_colors = ["skyblue", "salmon", "lightgreen"]
 
     fig, axs = plt.subplots(2)
-    need.T.plot(
-        kind="bar", width=0.2, title="Need in h per day", ax=axs[0], color='midnightblue'
+    needs.T.plot(
+        kind="bar",
+        width=0.2,
+        title="Need in h per day",
+        ax=axs[0],
+        color="midnightblue",
     )
 
-    plan[["Min capacity", "Max capacity"]].plot(
+    planning[["Min capacity", "Max capacity"]].plot(
         rot=90, ax=axs[1], style=["b", "b--"], linewidth=1
     )
 
-    plan.drop(["Min capacity", "Max capacity"], axis=1).plot(
+    planning.drop(["Min capacity", "Max capacity"], axis=1).plot(
         kind="bar", title="Load in h per line", ax=axs[1], color=my_colors
     )
 
@@ -106,38 +124,39 @@ def plot_planning(plan, need):
     axs[1].tick_params(axis="y", labelsize=7)
 
     plt.savefig("Result_Model1.png", bbox_inches="tight", dpi=1200)
-    axe = plt.show()
-    return axe
+    plt.show()
 
 
 # Generate inputs
 # Define the daily requirement
-CALENDAR: List[str] = [
-    "2020/7/13",
-    "2020/7/14",
-    "2020/7/15",
-    "2020/7/16",
-    "2020/7/17",
-    "2020/7/18",
-    "2020/7/19",
-]
-day_req: List[int] = [30, 10, 34, 23, 23, 24, 25]
-DAY_REQUIREMENTS: Dict[str, int] = {day: day_req[i] for i, day in enumerate(CALENDAR)}
-df_requirement = pd.DataFrame.from_dict({day: [day_req[i]] for i, day in enumerate(CALENDAR)})
+
+daily_requirements: Dict[str, int] = {
+    "2020/7/13": 30,
+    "2020/7/14": 10,
+    "2020/7/15": 34,
+    "2020/7/16": 23,
+    "2020/7/17": 23,
+    "2020/7/18": 24,
+    "2020/7/19": 25,
+}
+
+calendar: List[str] = list(daily_requirements.keys())
+daily_requirements_df = pd.DataFrame.from_dict(daily_requirements)
 
 # Define the hourly cost per line
-LINES: List[str] = ["Curtain_C1", "Curtain_C2", "Curtain_C3"]
-cost_temp: List[int] = [350, 300, 350]
-COST_LINE = {line: cost_temp[i] for i, line in enumerate(LINES)}
+costs_per_line = {"Curtain_C1": 350, "Curtain_C2": 300, "Curtain_C3": 350}
+lines: List[str] = list(costs_per_line.keys())
 
 # Optimize the planning
-solution = optimize_planning(CALENDAR, LINES, DAY_REQUIREMENTS, COST_LINE)
+solution = optimize_planning(calendar, lines, daily_requirements, costs_per_line)
 
 # Format the result
-optimized_planning = pd.DataFrame(index=LINES, columns=CALENDAR)
-for line in LINES:
-    for day in CALENDAR:
-        optimized_planning.at[line, day] = solution.loc["Total hours[" + str(day) + "," + str(line) + "]"][0]
+optimized_planning = pd.DataFrame(index=lines, columns=calendar)
+for line in lines:
+    for day in calendar:
+        optimized_planning.at[line, day] = solution.loc[
+            "Total hours[" + str(day) + "," + str(line) + "]"
+        ][0]
 
 # Plot the new planning
-plot_planning(optimized_planning, df_requirement)
+plot_planning(optimized_planning, daily_requirements_df)
